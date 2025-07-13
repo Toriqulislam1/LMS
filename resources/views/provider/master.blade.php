918470c3c9ceddf7fd77cbb5ca21be45bc339d88
Toriqul Islam <96937038+Toriqulislam1@users.noreply.github.com>
2025-07-13 12:27:07 +0600

    some added

--- resources/views/provider/master.blade.php
+++ resources/views/provider/master.blade.php
@@ -9,7 +9,7 @@
     />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>
-        TailAdmin
+        Provider
     </title>
      <script src="https://cdn.tailwindcss.com"></script>
   </head>
@@ -52,7 +52,6 @@
                 <!-- Metric Group One -->
                 @include('partials.metric-group.metric-group-01')
                 <!-- Metric Group One -->
-
                 <!-- ====== Chart One Start -->
                 @include('partials.chart.chart-01')
                 <!-- ====== Chart One End -->
@@ -62,19 +61,16 @@
                 @include('partials.chart.chart-02')
                 <!-- ====== Chart Two End -->
               </div>
-
               <div class="col-span-12">
                 <!-- ====== Chart Three Start -->
                 @include('partials.chart.chart-03')
                 <!-- ====== Chart Three End -->
               </div>
-
               <div class="col-span-12 xl:col-span-5">
                 <!-- ====== Map One Start -->
                 @include('partials.map-01')
                 <!-- ====== Map One End -->
               </div>
-
               <div class="col-span-12 xl:col-span-7">
                 <!-- ====== Table One Start -->
                 @include('partials.table.table-01')
--- routes/provider.php
+++ routes/provider.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('provider')->group(function () {
+    require base_path('app/Modules/Provider/routes.php');
 });
