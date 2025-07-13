3d1678187319506ef179ac13bffcd3d3d5d990ba
Toriqul Islam <96937038+Toriqulislam1@users.noreply.github.com>
2025-07-13 11:07:42 +0600

    aasdf

---
+++ resources/views/provider/partials/sidebar.blade.php
@@ -0,0 +1,95 @@
+<script src="https://cdn.tailwindcss.com"></script>
+<script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
+
+<aside
+    :class="sidebarToggle ? 'translate-x-0 lg:w-[90px]' : '-translate-x-full'"
+    class="sidebar fixed left-0 top-0 z-[9999] flex h-screen w-[290px] flex-col overflow-y-hidden border-r border-gray-200 bg-white px-5 dark:bg-white dark:border-gray-800 lg:static lg:translate-x-0 transition-transform duration-300 ease-in-out"
+>
+    <!-- SIDEBAR HEADER -->
+    <div :class="sidebarToggle ? 'justify-center' : 'justify-between'"
+        class="flex items-center gap-2 pt-8 sidebar-header pb-7">
+        <a href="index.html" class="flex items-center">
+            <span class="logo" :class="sidebarToggle ? 'hidden' : ''">
+                <img class="dark:hidden" src="{{ asset('images/logo/logo.svg') }}" alt="Logo" />
+                <img class="hidden dark:block" src="{{ asset('images/logo/logo-dark.svg') }}" alt="Logo" />
+            </span>
+
+            <img class="logo-icon" :class="sidebarToggle ? 'lg:block' : 'hidden'"
+                src="{{ asset('images/logo/logo-icon.svg') }}" alt="Logo" />
+        </a>
+    </div>
+    <!-- END SIDEBAR HEADER -->
+
+    <div class="flex flex-col overflow-y-auto duration-300 ease-linear no-scrollbar">
+        <!-- Sidebar Menu -->
+        <nav x-data="{ selected: $persist('Dashboard') }">
+            <!-- Menu Group -->
+            <div>
+                <h3 class="mb-4 text-xs uppercase leading-[20px] text-gray-400">
+                    <span class="menu-group-title" :class="sidebarToggle ? 'lg:hidden' : ''">
+                        MENU
+                    </span>
+
+                    <svg :class="sidebarToggle ? 'lg:block hidden' : 'hidden'"
+                        class="mx-auto fill-current menu-group-icon" width="24" height="24" viewBox="0 0 24 24"
+                        fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd"
+                            d="M5.99915 10.2451C6.96564 10.2451 7.74915 11.0286 7.74915 11.9951V12.0051C7.74915 12.9716 6.96564 13.7551 5.99915 13.7551C5.03265 13.7551 4.24915 12.9716 4.24915 12.0051V11.9951C4.24915 11.0286 5.03265 10.2451 5.99915 10.2451ZM17.9991 10.2451C18.9656 10.2451 19.7491 11.0286 19.7491 11.9951V12.0051C19.7491 12.9716 18.9656 13.7551 17.9991 13.7551C17.0326 13.7551 16.2491 12.9716 16.2491 12.0051V11.9951C16.2491 11.0286 17.0326 10.2451 17.9991 10.2451ZM13.7491 11.9951C13.7491 11.0286 12.9656 10.2451 11.9991 10.2451C11.0326 10.2451 10.2491 11.0286 10.2491 11.9951V12.0051C10.2491 12.9716 11.0326 13.7551 11.9991 13.7551C12.9656 13.7551 13.7491 12.9716 13.7491 12.0051V11.9951Z"
+                            fill="" />
+                    </svg>
+                </h3>
+            </div>
+
+            <div>
+                <ul class="flex flex-col gap-4 mb-6">
+
+                    <!-- Menu Item with submenu -->
+                    <li x-data="{ open: true }" class="relative">
+                        <a href="#" @click.prevent="open = !open"
+                            class="menu-item group flex items-center justify-between text-gray-700 dark:text-gray-900"
+                            :class="open ? 'menu-item-active' : 'menu-item-inactive'">
+
+                            <div class="flex items-center gap-2">
+                                <svg :class="open ? 'text-primary' : 'text-gray-400 dark:text-gray-300'" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" clip-rule="evenodd"
+                                        d="M14 2.75C14 2.33579 14.3358 2 14.75 2C15.1642 2 15.5 2.33579 15.5 2.75V5.73291H19C19.4142 5.73291 19.75 6.0687 19.75 6.48291C19.75 6.89712 19.4142 7.23291 19 7.23291H18.5V12.2329C18.5 15.5691 15.9866 18.3183 12.75 18.6901V21.25C12.75 21.6642 12.4142 22 12 22C11.5858 22 11.25 21.6642 11.25 21.25V18.6901C8.01342 18.3183 5.5 15.5691 5.5 12.2329V7.23291H5C4.58579 7.23291 4.25 6.89712 4.25 6.48291C4.25 6.0687 4.58579 5.73291 5 5.73291H10V2.75C10 2.33579 10.3358 2 10.75 2C11.1642 2 11.5 2.33579 11.5 2.75V5.73291H14ZM7 7.23291V12.2329C7 14.9943 9.23858 17.2329 12 17.2329C14.7614 17.2329 17 14.9943 17 12.2329V7.23291H7Z"
+                                        fill="currentColor" />
+                                </svg>
+                                <span class="menu-item-text">Authentication</span>
+                            </div>
+
+                            <svg :class="open ? 'rotate-180' : ''"
+                                class="stroke-current" width="20" height="20"
+                                viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M4.79175 7.39584L10.0001 12.6042L15.2084 7.39585" stroke="currentColor"
+                                    stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                        </a>
+
+                        <!-- Dropdown Menu -->
+                        <div x-show="open" x-transition x-cloak class="mt-2 pl-9">
+                            <ul class="flex flex-col gap-1">
+                                <li>
+                                    <a href="signin.html"
+                                        class="block px-2 py-1 text-sm text-gray-600 dark:text-gray-700 hover:text-primary">
+                                        Sign In
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="signup.html"
+                                        class="block px-2 py-1 text-sm text-gray-600 dark:text-gray-700 hover:text-primary">
+                                        Sign Up
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <!-- End Menu Item Authentication -->
+
+                </ul>
+            </div>
+        </nav>
+        <!-- End Sidebar Menu -->
+    </div>
+</aside>
