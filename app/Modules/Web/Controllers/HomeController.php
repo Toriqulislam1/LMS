0098abca1d11622d292d9242e529fb3bd56bace4
Toriqul Islam <96937038+Toriqulislam1@users.noreply.github.com>
2025-07-26 12:38:59 +0600

    home added

--- app/Modules/Web/Controllers/HomeController.php
+++ app/Modules/Web/Controllers/HomeController.php
@@ -9,6 +9,46 @@ class HomeController extends Controller
 {
     public function home()
     {
-        return view('web.home'); // Make sure this view exists
+        return view('web.home');
+    }
+    public function about()
+    {
+        return view('web.about');
+    }
+    public function contact()
+    {
+        return view('web.contact');
+    }
+    public function course()
+    {
+        return view('web.course');
+    }
+    public function blog()
+    {
+        return view('web.blog');
+    }
+    public function blogPost($slug)
+    {
+        return view('web.blog-post', ['slug' => $slug]);
+    }
+    public function shop()
+    {
+        return view('web.shop');
+    }
+    public function terms()
+    {
+        return view('web.terms');
+    }
+    public function privacy()
+    {
+        return view('web.privacy');
+    }
+    public function sitemap()
+    {
+        return view('web.sitemap');
+    }
+    public function error404()
+    {
+        return view('web.error404');
     }
 }
